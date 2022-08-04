36d1d4e85e804e2991c85e17058cd6e6a90052de
Shohruh003 <shohruhazimov003@gmail.com>
2022-08-05 01:56:27 +0500

    Add function

--- src/App.js
+++ src/App.js
@@ -1,91 +1,20 @@
 import Header from "./components/Header";
 import Search from "./components/Search";
-import Card from "./components/Card";
+
 import "./App.css"
-import {useState} from "react";
+
 
 
 function App() {
 
-  const [countries, setCountries] = useState([
-    {
-      id: 1,
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/2560px-Flag_of_Germany.svg.png",
-      name: "Germany",
-      population: "81,770,900",
-      region: "Europe",
-      capital: "Berlin"
-    },
-    {
-      id: 2,
-      img: "https://nationalflagfoundation.org/wp-content/uploads/1795.png",
-      name: "Germany",
-      population: "323,947,000",
-      region: "Americas",
-      capital: "Washington D.C"
-    },
-    {
-      id: 3,
-      img: "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/640px-Flag_of_Brazil.svg.png",
-      name: "Brazil",
-      population: "206,135,893",
-      region: "Americas",
-      capital: "Brasília"
-    },
-    {
-      id: 4,
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Iceland.svg/1024px-Flag_of_Iceland.svg.png",
-      name: "Iceland",
-      population: "334,300",
-      region: "Europe",
-      capital: "Reykjavík"
-    },
-    {
-      id: 5,
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Flag_of_Afghanistan_%282013%E2%80%932021%29.svg/1024px-Flag_of_Afghanistan_%282013%E2%80%932021%29.svg.png",
-      name: "Afghanistan",
-      population: "27,657,145",
-      region: "Asia",
-      capital: "Kabul"
-    },
-    {
-      id: 6,
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Flag_of_Uzbekistan.svg/1280px-Flag_of_Uzbekistan.svg.png",
-      name: "Uzbekistan",
-      population: "35,482,369",
-      region: "Asia",
-      capital: "Tashkent"
-    },
-    {
-      id: 7,
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Flag_of_Albania.svg/2560px-Flag_of_Albania.svg.png",
-      name: "Albania",
-      population: "2,886,026",
-      region: "Europe",
-      capital: "Tirana"
-    },
-    {
-      id: 8,
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Flag_of_Algeria.svg/1280px-Flag_of_Algeria.svg.png",
-      name: "Algeria",
-      population: "40,400,000",
-      region: "Africa",
-      capital: "Algiers"
-    },
-  ])
+
 
   return (
     <div>
       <Header/>
       <div className="container">
       <Search/>
-      <ul className="cardList">
-        {
-          countries.map(e => (
-            <Card name={e.name} population={e.population} src={e.img} region={e.region} capital={e.capital}/>
-          ))
-        }
-      </ul>
+      
     </div>
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,24 +1,83 @@
 import "./Search.css";
 import SearchImg from "../img/search.png";
+import Card from "./Card";
+import { useEffect, useState } from "react";
 
 const Search = () => {
+  const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState("");
+  const [region, setRegion] = useState("");
+
+  useEffect(() => {
+    if (value.length) {
+      fetch(`https://restcountries.com/v3.1/name/${value}`)
+        .then((res) => res.json())
+        .then((data) => setTodos(data))
+        .catch((er) => console.log(er));
+    } else if (region !== "Filter by region" && region.length) {
+      fetch(`https://restcountries.com/v3.1/region/${region}`)
+        .then((res) => res.json())
+        .then((data) => setTodos(data))
+        .catch((er) => console.log(er));
+    } else {
+      fetch(`https://restcountries.com/v3.1/all`)
+        .then((res) => res.json())
+        .then((data) => setTodos(data))
+        .catch((er) => console.log(er));
+    }
+  }, [value, region]);
+
   return (
-    <form className="form" method="post">
+    <div>
+      <div className="form">
         <label className="label">
-          <img src={SearchImg} alt="search img" width="20" height="20"/>
-          <input className="input" type="text" placeholder="Search for a country..."/>
+          <img src={SearchImg} alt="search img" width="20" height="20" />
+          <input
+            onKeyUp={(evt) => {
+              if (evt.code === "Enter") {
+                setValue(evt.target.value);
+              }
+            }}
+            className="input"
+            type="text"
+            placeholder="Search for a country..."
+          />
         </label>
 
-        <select className="select">
-        <option value="" selected disabled>Filter by Region</option>
-        <option value="Africa">Africa</option>
-        <option value="America">America</option>
-        <option value="Asia">Asia</option>
-        <option value="Europe">Europe</option>
-        <option value="Oceanie">Oceanie</option>
+        <select
+          defaultValue={"Filter by Region"}
+          onChange={(evt) => {
+            setRegion(evt.target.value);
+          }}
+          className="select"
+        >
+          <option disabled>Filter by Region</option>
+          <option defaultValue="Africa">Africa</option>
+          <option defaultValue="America">America</option>
+          <option defaultValue="Asia">Asia</option>
+          <option defaultValue="Europe">Europe</option>
+          <option defaultValue="Oceanie">Oceanie</option>
         </select>
-    </form>
-  )
-}
+      </div>
+
+      <h1>{value}</h1>
+
+      <ul className="cardList">
+        {todos.length
+          ? todos.map((e) => (
+              <Card
+                key={e.id}
+                name={e.name.common}
+                population={e.population}
+                src={e.flags.png}
+                region={e.region}
+                capital={e.capital}
+              />
+            ))
+          : "Loading..."}
+      </ul>
+    </div>
+  );
+};
 
 export default Search;
